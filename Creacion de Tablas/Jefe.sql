5f2b1e754603f307556c47ab9dd07add106e4035
Adanale44 <164179670+Adanale44@users.noreply.github.com>
2024-11-01 02:09:12 -0300

    Update Jefe.sql
    error al escribir las tablas

--- Creacion de Tablas/Jefe.sql
+++ Creacion de Tablas/Jefe.sql
@@ -1,7 +1,8 @@
 CREATE TABLE jefes (
     jefe_id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre_jefe VARCHAR(255) NOT NULL,
-    salud_jefe INT,  
+    nombre_jefe VARCHAR(50) NOT NULL,
+    salud INT,  
+    movilidad VARCHAR(50) NOT NULL,
     puede_ser_invocado BOOLEAN,
     requisitos_invocacion VARCHAR(255),
     loot TEXT
