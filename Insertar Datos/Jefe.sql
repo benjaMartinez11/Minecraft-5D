0948752e23c614420ba2226a55f38139d28501bf
Adanale44 <164179670+Adanale44@users.noreply.github.com>
2024-11-01 02:06:00 -0300

    Update Jefe.sql
    modificación de tabla, borramos algunas cosas que no se podían definir bien y las hicimos en otra tabla

--- Insertar Datos/Jefe.sql
+++ Insertar Datos/Jefe.sql
@@ -1,16 +1,15 @@
-INSERT INTO jefes (nombre_jefe, salud,daño_min, daño_max, ataque_distancia, ataque_mele
-  , habilidad_especial, puede_ser_invocado, requisitos_invocacion, loot
+INSERT INTO jefes (nombre_jefe, salud, movilidad, puede_ser_invocado,
+  , requisitos_invocacion, loot
 ) VALUES
 (
-    'Ender Dragon', 200, 3, 10, TRUE, TRUE, "Bola de fuego",
-    'Cristales de Ender','Aliento de Dragón, Huevo de Dragón'
+    "Ender Dragon", 200, "volar", TRUE, "Cristales de Ender", 
+    "Huevo de Dragón"
 ),
 (
-    'Wither', 450, 8, 68, TRUE, TRUE, "Cabeza de whiter",
-    '3 cabezas de esqueleto Wither, 4 bloques de arena de almas',
-    'Estrella de Nether'
+    "Wither", 300, "volar", TRUE, "3 cabezas de esqueleto Wither, 4 bloques de arena de almas",
+    "Estrella de Nether"
 ),
 (
-    'Elder Guardian', 80, 3, 8, TRUE, FALSE, "Rayo laser",
-    NULL,'Prismarina'
+    "Elder Guardian", 80, "nadar", FALSE, NULL,
+    "Prismarina"
 );
