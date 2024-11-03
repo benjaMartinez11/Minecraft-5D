730a0d57f9ed4e3250c15b81a9fbcc04411d8af5
Adanale44 <164179670+Adanale44@users.noreply.github.com>
2024-11-03 02:51:49 -0300

    Create mob_neutro.sql
    Tabla para los npcs neutrales, esta tabla puede juntarse con la de mobs

---
+++ Creacion de Tablas/mob_neutro.sql
@@ -0,0 +1,10 @@
+CREATE TABLE mob_neutro( 
+  ID INT PRIMARY KEY AUTO_INCREMENT,
+  Nombre_neutro VARCHAR(50) NOT NULL,
+  loot VARCHAR(50) NOT NULL,
+  loot_exp BOOLEAN NOT NULL,
+  reproduccion BOOLEAN NOT NULL,
+  ataque INT NOT NULL,
+  vida DECIMAL),
+  movilidad VARCHAR(50) NOT NULL,
+)
