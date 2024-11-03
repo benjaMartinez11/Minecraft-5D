449285c499e1128f093d4036b40ae85fe5122fd4
Adanale44 <164179670+Adanale44@users.noreply.github.com>
2024-11-03 03:31:35 -0300

    Insolación de datos mob_neutro

---
+++ Insertar Datos/mob_neutro.sql
@@ -0,0 +1,5 @@
+INSERT INTO mob_neutro(nombre_pass , loot, loot_exp, ataque, movilidad, reproduccion, vida_neutro);
+VALUES ("Enderman", "Enderperl", true, 3.5, "Terrestre", false, 40.0),
+  ("Golem de hierro", "Hierro, flor roja", true, 7.5, "Terrestre", false, 100.0)
+  ("Lobo", null, true, 2.0, "Terrestre", false, 4.0),
+  ("Lobo domesticado de hierro", null, true, 2.0, "Terrestre", false, 10.0)
