e84bc4160a08543812b2f14c474fa7abece42a8c
benjamin martinez <benjamartinez454gmail.com>
2024-11-06 09:49:32 -0300

    hice una modificacion en los inserts

--- Insertar Datos/Estructura.sql
+++ Insertar Datos/Estructura.sql
@@ -1,12 +1,12 @@
-INSERT INTO estructuras (nombre_estructura, bioma_id, dimension_id, tipo_de_estructura)
-VALUES ("templo marino","bioma","dimension","tipo de estructura"),
-("Templo del desierto","bioma","dimension","tipo de estructura"),
-("Templo de la jungla","bioma","dimension","tipo de estructura"),
-("Aldeas","bosque","dimension","tipo de estructura"),
-("Mazmorras","bioma","dimension","tipo de estructura"),
-("Fortaleza","x","dimension","tipo de estructura"),
-("Pozo en el desierto","desierto","dimension","tipo de estructura"),
-("pico del end","bioma","x","tipo de estructura"),
-("fortaleza del nether","x","dimension","tipo de estructura"),
-("casa de bruja","pantano","dimension","tipo de estructura"),
-("mina abandonada","bosque","dimension","tipo de estructura");
+INSERT INTO estructuras (nombre_estructura, bioma_id, dimension_id)
+VALUES ("templo marino","bioma","dimension"),
+("Templo del desierto","bioma","dimension"),
+("Templo de la jungla","bioma","dimension"),
+("Aldeas","bosque","dimension"),
+("Mazmorras","bioma","dimension"),
+("Fortaleza","x","dimension"),
+("Pozo en el desierto","desierto","dimension"),
+("pico del end","bioma","x"),
+("fortaleza del nether","x","dimension"),
+("casa de bruja","pantano","dimension"),
+("mina abandonada","bosque","dimension");
