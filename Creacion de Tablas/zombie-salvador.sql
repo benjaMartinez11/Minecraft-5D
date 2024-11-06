df8e5e094574277cfd8dfb068cd97231a5f8b20b
Briancorbalan1801 <bcorbalanbenitez@gmail.com>
2024-11-06 10:45:45 -0300

    Cree la tabla zombie salvador

---
+++ Creacion de Tablas/zombie-salvador.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Zombie_salvador (
+    ID INT PRIMARY KEY,
+    items_ID INT,
+    mob_id INT,
+    FOREIGN KEY (items_ID) REFERENCES Item(ID),
+    FOREIGN KEY (mob_id) REFERENCES mob(ID)
+);
