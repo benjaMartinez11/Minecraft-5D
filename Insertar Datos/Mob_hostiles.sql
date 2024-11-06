83cbde9fdbd0612417eda21e6ffab2507b65726f
benjaminvasquez07 <benjaminvasquez289@gmail.com>
2024-11-06 10:54:22 -0300

    hice el insert de mob hostil

---
+++ Insertar Datos/Mob_hostiles.sql
@@ -0,0 +1,12 @@
+INSERT INTO mob_hostil(Nombre_hostil, loot, loot_exp, ataque, vida, movilidad, disctancia_de_ataque)
+VALUES
+  ("Zombi", "Carne, cuero", true, 3, 10.0, "Terrestre", "Corto"),
+  ("Esqueleto", "Arco, flechas", true, 4, 10.0, "Terrestre", "Distancia"),
+  ("Creeper", "Gunpowder", true, 0, 10.0, "Terrestre", "Corto"),
+  ("Enderman", "Enderperl", true, 7, 20.0, "Terrestre", "Corto"),
+  ("Bruja", "Pociones, polvo de Blaze", true, 0, 13.0, "Terrestre", "Corto"),
+  ("Magma Cube", "Magma, polvo de Blaze", true, 4, 8.0, "Terrestre", "Corto"),
+  ("Blaze", "Polvo de Blaze, varita de Blaze", true, 5, 10.0, "Aéreo", "Distancia"),
+  ("Ghast", "Piedra luminosa, pólvora", true, 6, 5.0, "Aéreo", "Distancia"),
+  ("Zombie Pigman", "Oro, carne", true, 7, 10.0, "Terrestre", "Corto"),
+  ("Guardian", "Esencia de Guardian, prismarina", true, 6, 15.0, "Acuático", "Corto");
