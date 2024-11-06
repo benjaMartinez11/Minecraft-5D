5c9e2d5c158ccc462ac9b5a1cd547449ef954373
Daniel-505 <fericos190@gmai.com>
2024-11-06 14:27:46 -0300

    hice el insert de encantamientos

---
+++ Insertar Datos/encantaminetos.sql
@@ -0,0 +1,8 @@
+INSERT INTO encantamientos (nombre_encantamiento)
+    VALUES ("Proteccion"),("Proteccion contra el fuego"),("proteccion contra explociones"),
+    ("proteccion contra proyectiles"),("caida de pluma"),("irrompibilidad"),
+    ("reparacion"),("espinas"),("filo"),("castigo"),("maldicion"),("empuje"),("aspecto"),
+    ("poder"),("empalamiento"),("conductividad"),("perforacion"),("multidisparo"),
+    ("carga rapida"),("retroceso"),("fuego"),("infinidad"),("lealtad"),("respiracion"),
+    ("afinidad acuatica"),("agilidad acuatica"),("paso helado"),("propulcion"),
+    ("eficiencia"),("botin"),("toque"),("fortuna"),("señuelo"),
