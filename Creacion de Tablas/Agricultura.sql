53b69242bfdfd883a039506c4c9ba0c8d8805f74
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-11-07 08:21:52 -0300

    Cambie un tipo de valor en agricultura

--- Creacion de Tablas/Agricultura.sql
+++ Creacion de Tablas/Agricultura.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Agricultura (
     ID INT PRIMARY KEY AUTO_INCREMENT,
     nombre_semilla VARCHAR(255),
-    tiempo_de_crecimiento INT,
+    tiempo_de_crecimiento Varchar(50),
     Loot VARCHAR(50),
     Interaccion BOOLEAN
 );
