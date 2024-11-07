ed63fe6061766c7c99e8b654ad8e4ba8301eb821
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-11-07 08:25:07 -0300

    Modifique el codigo para insertar Armadura

--- Insertar Datos/Armadura.sql
+++ Insertar Datos/Armadura.sql
@@ -1,13 +1,13 @@
 INSERT INTO Armadura(Nombre_armadura, de_que_esta_hecho, durabilidad, defensa, durabilidad_unidad)
  VALUES
 ("Casco de cuero","5 de cuero", 55, 0.5,'68%'),("Pechera de cuero","8 de cuero", 80, 1.5,'61%'),
-("Pantalon de cuero","7 de cuero", 75, 1.0),("Botas de cuero","4 de cuero", 65, 0.5),
+("Pantalon de cuero","7 de cuero", 75, 1.0, '68%'),("Botas de cuero","4 de cuero", 65, 0.5, '68%'),
   
 ("Casco de oro","5 de oro", 77, 1.0,'68%'),("Pechera de oro","8 de oro", 112, 2.5,'61%'),
 ("Pantalon de oro","7 de oro", 105, 1.5,'66%'),("Botas de oro","4 de oro", 91, 0.5,'100%'),
   
-("Casco de cota de malla ","5 de fuego", 165, 1.0,'68%'),("Pechera de cota de malla","8 de fuego", 240, 2.5,'61%'), 165	240	225	195
-("Pantalon de cota de malla","7 de fuego", 225, 2.0),("Botas de cota de malla","4 de fuego", 195, 0.5,'100%'),
+("Casco de cota de malla ","5 de fuego", 165, 1.0,'68%'),("Pechera de cota de malla","8 de fuego", 240, 2.5,'61%'), 
+("Pantalon de cota de malla","7 de fuego", 225, 2.0,'68%'),("Botas de cota de malla","4 de fuego", 195, 0.5,'100%'),
   
 ("Casco de hierro","5 de hierro", 165, 1.0,'68%'),("Pechera de hierro","8 de hierro", 240, 3.0,'61%'),
 ("Pantalon de hierro","7 de hierro", 225, 2.5,'66%'),("Botas de hierro","4 de hierro", 195, 1.0,'100%'),
