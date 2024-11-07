0b2f36e2d0048b8d873b97d1da3997c581756087
BrianCorbalan1801 <164352595+BrianCorbalan1801@users.noreply.github.com>
2024-11-07 08:13:12 -0300

    Agregue el auto incremental en Herramientas.sql

--- Creacion de Tablas/Herramientas.sql
+++ Creacion de Tablas/Herramientas.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Herramientas (
-    ID INT PRIMARY KEY,     
+    ID INT PRIMARY KEY AUTO_INCREMENT,     
     nombre_herramienta VARCHAR(50), 
     como_se_hace VARCHAR(50),     
     crafteo VARBINARY(255)
