7183309ea5c30055ac6115952f2d564b08cc6a35
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-11-07 08:46:26 -0300

    Corregi error que no estaba referenciando a la tabla Mobs

--- Creacion de Tablas/Mob-dimension.sql
+++ Creacion de Tablas/Mob-dimension.sql
@@ -2,6 +2,6 @@ CREATE TABLE Mob_dimension (
     ID INT PRIMARY KEY AUTO_INCREMENT,
     mob_id INT,
     dimension_id INT,
-    FOREIGN KEY (mob_id) REFERENCES Mob(ID),
+    FOREIGN KEY (mob_id) REFERENCES Mobs(ID),
     FOREIGN KEY (dimension_id) REFERENCES Dimension(ID)
 );
