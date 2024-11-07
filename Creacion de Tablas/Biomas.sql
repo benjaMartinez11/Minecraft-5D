401fd1974afd40b9e30f01acf5ad328d3479e95e
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-11-07 08:27:33 -0300

    Modifique el codigo para insertar Biomas

--- Creacion de Tablas/Biomas.sql
+++ Creacion de Tablas/Biomas.sql
@@ -2,6 +2,5 @@ CREATE TABLE Bioma (
     ID INT PRIMARY KEY AUTO_INCREMENT,
     nombre_bioma VARCHAR(255) NOT NULL,
     generar_mob_pass BOOLEAN,
-    generar_mob_hoss BOOLEAN,
-    descripcion text
+    generar_mob_hoss BOOLEAN
 );
