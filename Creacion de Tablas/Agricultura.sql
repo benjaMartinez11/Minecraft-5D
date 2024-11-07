9a97e40263044f1909c50aea720c971dec08411b
BrianCorbalan1801 <164352595+BrianCorbalan1801@users.noreply.github.com>
2024-11-07 08:12:10 -0300

    Actualizar Agricultura.sql por error mio

--- Creacion de Tablas/Agricultura.sql
+++ Creacion de Tablas/Agricultura.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Agricultura (
-    ID INT PRIMARY KEY AUTO INCREMENT,
+    ID INT PRIMARY KEY AUTO_INCREMENT,
     nombre_semilla VARCHAR(255),
     tiempo_de_crecimiento INT,
     Loot VARCHAR(50),
