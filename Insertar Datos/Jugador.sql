742a9abd492359b19c60c9841d802a250a911da8
Adna <dbysc1214@gmail.com>
2024-11-13 17:16:01 -0300

    Hice las rutas faltantes y puse el DELETE, POST y PUT, pero falta ponerlos en algunos

--- Insertar Datos/Jugador.sql
+++ Insertar Datos/Jugador.sql
@@ -3,3 +3,6 @@ INSERT INTO Jugador (nombre_usuario, ataque, fecha_registro, salud)
     ("Steve", 0.5, "16 de Mayo de 2009", 20),
     ("Alex", 0.5, "16 de Mayo de 2009", 20);
 
+    ("iPanditaC_", 0.5, "16 de Mayo de 2009", 20),
+    ("Mico0102", 0.5, "16 de Mayo de 2009", 20);
+
--- Mostrar Tablas/app.py
+++ Mostrar Tablas/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import mariadb
+from flask import mariadb
 
 app = Flask(__name__)
 
@@ -22,6 +22,7 @@ def listar_item():
 
     return jsonify(tabla)
 
+#====================================================
 @app.route("/armadura")
 def listar_armaduras():
     mari = mariadb.connect(
@@ -106,6 +107,8 @@ def modificar_armadura(id):
     return jsonify({"resultado" : "ok",
                     "id" : id})
 
+#====================================================
+#====================================================
 @app.route("/mob")
 def listar_mobs():
     mari = mariadb.connect(
@@ -119,6 +122,9 @@ def listar_mobs():
     tabla = cur.fetchall()
 
     return jsonify(tabla)
+
+#====================================================
+#====================================================
 
 @app.route("/mob_pass")
 def listar_mobs_pass():
@@ -177,7 +183,7 @@ def agregar_mob_pass():
                     "id" : id})
 
 @app.route("/mob_pass/<int:id>", methods=('PUT',))
-def modificar_armadura(id):
+def modificar_mob_pass(id):
     mari = mariadb.connect(
         user = "minecraft",
         password ="minecraft111",
@@ -204,8 +210,8 @@ def modificar_armadura(id):
 
     return jsonify({"resultado" : "ok",
                     "id" : id})
-
-
+#====================================================
+#====================================================
 @app.route("/mob_hostil")
 def listar_mobs_hostil():
     mari = mariadb.connect(
@@ -221,7 +227,7 @@ def listar_mobs_hostil():
     return jsonify(tabla)
 
 @app.route("/mob_hostil/<int:id>", methods=('DELETE',))
-def borrar_armadura(id):
+def borrar_mob_hostil(id):
     mari = mariadb.connect(
         user = "minecraft",
         password ="minecraft111",
@@ -236,7 +242,7 @@ def borrar_armadura(id):
                     "id" : id})
 
 @app.route("/mob_hostil/", methods=('POST',))
-def agregar_armadura():
+def agregar_mob_hostil():
     mari = mariadb.connect(
         user = "minecraft",
         password ="minecraft111",
@@ -245,24 +251,26 @@ def agregar_armadura():
     )
     cur = mari.cursor(dictionary=True)
     nombre = request.json["nombre"]
-    material = request.json["material"]
-    durabilidad = request.json["durabilidad"]
-    defensa = request.json["defensa"]
-    dur_uni = request.json["durabilidad_unidad"]
+    loot = request.json["loot"]
+    loot_exp = request.json["loot_exp"]
+    movilidad = request.json["mivilidad"]
+    ataque = request.json["ataque"]
+    vida = request.json["vida"]
+    distacia = request.json["distancia"]
     consulta = """
-        INSERT INTO Mob_pass (Nombre_armadura, de_que_esta_hecho, durabilidad, 
-            defensa, durabilidad_unidad) VALUES (%s, %s, %s, %s, %s)
+        INSERT INTO Mob_hostil (Nombre_hostil, loot, loot_exp, 
+            movilidad, ataque, vida, distacia_de_atque) VALUES (%s, %s, %s, %s, %s)
 """
 
-    cur.execute(consulta, (nombre, material, durabilidad, defensa, dur_uni,))
+    cur.execute(consulta, (nombre, loot, loot_exp, movilidad, ataque, vida, distacia,id))
     mari.commit()
     id = cur.lastrowid
 
     return jsonify({"resultado" : "ok",
                     "id" : id})
 
-@app.route("/Mob_pass/<int:id>", methods=('PUT',))
-def modificar_armadura(id):
+@app.route("/mob_hostil/<int:id>", methods=('PUT',))
+def modificar_mob_hostil(id):
     mari = mariadb.connect(
         user = "minecraft",
         password ="minecraft111",
@@ -271,16 +279,18 @@ def modificar_armadura(id):
     )
     cur = mari.cursor(dictionary=True)
     nombre = request.json["nombre"]
-    material = request.json["material"]
-    durabilidad = request.json["durabilidad"]
-    defensa = request.json["defensa"]
-    dur_uni = request.json["durabilidad_unidad"]
+    loot = request.json["loot"]
+    loot_exp = request.json["loot_exp"]
+    movilidad = request.json["mivilidad"]
+    ataque = request.json["ataque"]
+    vida = request.json["vida"]
+    distacia = request.json["distancia"]
     consulta = """
-        UPDATE Mob_pass SET Nombre_armadura = %s, de_que_esta_hecho = %s, 
-            durabilidad = %s, defensa=%s, durabilidad_unidad=%s
+        UPDATE Mob_hostil SET Nombre_hostil = %s, loot = %s, 
+            loot_exp = %s, movilidad=%s, ataque=%s, vida=%s, distancia_de_ataque=%s,
             WHERE id = %s;
 """
-    cur.execute(consulta, (nombre, material, durabilidad, defensa, dur_uni,id))
+    cur.execute(consulta, (nombre, loot, loot_exp, movilidad, ataque, vida, distacia,id))
     mari.commit()
 
     cur.close()
@@ -288,8 +298,8 @@ def modificar_armadura(id):
 
     return jsonify({"resultado" : "ok",
                     "id" : id})
-
-
+#====================================================
+#====================================================
 @app.route("/dimension")
 def listas_dimension():
     mari = mariadb.connect(
@@ -304,6 +314,68 @@ def listas_dimension():
 
     return jsonify(tabla)
 
+@app.route("/dimension/<int:id>", methods=('DELETE',))
+def borrar_dimension(id):
+    mari = mariadb.connect(
+        user = "minecraft",
+        password ="minecraft111",
+        host ="10.9.120.5",
+        database= "minecraft"
+    )
+    cur = mari.cursor(dictionary=True)
+    cur.execute("DELETE FROM Dimension WHERE ID = %s", (id,))
+    mari.commit()
+
+    return jsonify({"resultado" : "ok",
+                    "id" : id})
+
+@app.route("/dimesion/", methods=('POST',))
+def agregar_dimension():
+    mari = mariadb.connect(
+        user = "minecraft",
+        password ="minecraft111",
+        host ="10.9.120.5",
+        database= "minecraft"
+    )
+    cur = mari.cursor(dictionary=True)
+    nombre = request.json["nombre"]
+    descripcion = request.json["descripcion"]
+    consulta = """
+        INSERT INTO Dimension (nombre_dimesion, descripcion_dimensino ) VALUES (%s, %s)
+"""
+
+    cur.execute(consulta, (nombre,descripcion,id))
+    mari.commit()
+    id = cur.lastrowid
+
+    return jsonify({"resultado" : "ok",
+                    "id" : id})
+
+@app.route("/dimension/<int:id>", methods=('PUT',))
+def modificar_dimension(id):
+    mari = mariadb.connect(
+        user = "minecraft",
+        password ="minecraft111",
+        host ="10.9.120.5",
+        database= "minecraft"
+    )
+    cur = mari.cursor(dictionary=True)
+    nombre = request.json["nombre"]
+    descripcion = request.json["descripcion"]
+    consulta = """
+        UPDATE Dimension SET nombre_dimension = %s, descripcion = %s,
+            WHERE id = %s;
+"""
+    cur.execute(consulta, (nombre, descripcion,id))
+    mari.commit()
+
+    cur.close()
+    mari.close()
+
+    return jsonify({"resultado" : "ok",
+                    "id" : id})
+#====================================================
+#====================================================
 @app.route("/agricultura")
 def listas_Agricultura():
     mari = mariadb.connect(
@@ -317,6 +389,72 @@ def listas_Agricultura():
     tabla = cur.fetchall()
 
     return jsonify(tabla)
+
+@app.route("/agricultura/<int:id>", methods=('DELETE',))
+def borrar_Agricultura(id):
+    mari = mariadb.connect(
+        user = "minecraft",
+        password ="minecraft111",
+        host ="10.9.120.5",
+        database= "minecraft"
+    )
+    cur = mari.cursor(dictionary=True)
+    cur.execute("DELETE FROM Agricultura WHERE ID = %s", (id,))
+    mari.commit()
+
+    return jsonify({"resultado" : "ok",
+                    "id" : id})
+
+@app.route("/agricultura/", methods=('POST',))
+def agregar_Agrilcultura():
+    mari = mariadb.connect(
+        user = "minecraft",
+        password ="minecraft111",
+        host ="10.9.120.5",
+        database= "minecraft"
+    )
+    cur = mari.cursor(dictionary=True)
+    nombre = request.json["nombre"]
+    tiempo = request.json["tiempo"]
+    loot = request.json["loot"]
+    interaccion = request.json["interaccion"]
+    consulta = """
+        INSERT INTO Agricultura (nombre_semilla, tiempo_de_crecimiento, Loot, Interaccion ) VALUES (%s, %s,%s, %s,%s)
+"""
+
+    cur.execute(consulta, (nombre,tiempo,loot, interaccion,id))
+    mari.commit()
+    id = cur.lastrowid
+
+    return jsonify({"resultado" : "ok",
+                    "id" : id})
+
+@app.route("/agricultura/<int:id>", methods=('PUT',))
+def modificar_agricultura(id):
+    mari = mariadb.connect(
+        user = "minecraft",
+        password ="minecraft111",
+        host ="10.9.120.5",
+        database= "minecraft"
+    )
+    cur = mari.cursor(dictionary=True)
+    nombre = request.json["nombre"]
+    tiempo = request.json["tiempo"]
+    loot = request.json["loot"]
+    interaccion = request.json["interaccion"]
+    consulta = """
+        UPDATE Dimension SET nombre_semilla = %s, tiempo_de_crecimiento = %s, Loot = %s, Interaccion = %s,
+            WHERE id = %s;
+"""
+    cur.execute(consulta, (nombre,tiempo,loot, interaccion,id))
+    mari.commit()
+    cur.close()
+    mari.close()
+
+    return jsonify({"resultado" : "ok",
+                    "id" : id})
+#====================================================
+#====================================================
 
 @app.route("/bioma")
 def lista_bioma():
@@ -332,6 +470,67 @@ def lista_bioma():
 
     return jsonify(tabla)
 
+@app.route("/bioma/<int:id>", methods=('DELETE',))
+def borrar_bioma(id):
+    mari = mariadb.connect(
+        user = "minecraft",
+        password ="minecraft111",
+        host ="10.9.120.5",
+        database= "minecraft"
+    )
+    cur = mari.cursor(dictionary=True)
+    cur.execute("DELETE FROM Bioma WHERE ID = %s", (id,))
+    mari.commit()
+
+    return jsonify({"resultado" : "ok",
+                    "id" : id})
+
+@app.route("/bioma/", methods=('POST',))
+def agregar_bioma():
+    mari = mariadb.connect(
+        user = "minecraft",
+        password ="minecraft111",
+        host ="10.9.120.5",
+        database= "minecraft"
+    )
+    cur = mari.cursor(dictionary=True)
+    nombre = request.json["nombre"]
+    consulta = """
+        INSERT INTO Agricultura (nombre_bioma ) VALUES (%s)
+"""
+
+    cur.execute(consulta, (nombre,id))
+    mari.commit()
+    id = cur.lastrowid
+
+    return jsonify({"resultado" : "ok",
+                    "id" : id})
+
+@app.route("/bioma/<int:id>", methods=('PUT',))
+def modificar_bioma(id):
+    mari = mariadb.connect(
+        user = "minecraft",
+        password ="minecraft111",
+        host ="10.9.120.5",
+        database= "minecraft"
+    )
+    cur = mari.cursor(dictionary=True)
+    nombre = request.json["nombre"]
+    consulta = """
+        UPDATE Dimension SET nombre_semilla = %s,
+            WHERE id = %s;
+"""
+    cur.execute(consulta, (nombre,id))
+    mari.commit()
+    cur.close()
+    mari.close()
+
+    return jsonify({"resultado" : "ok",
+                    "id" : id})
+
+#====================================================
+#====================================================
+
 @app.route("/caracteristicas_jefe")
 def lista_caracteristicas_jefes():
     mari = mariadb.connect(
@@ -345,6 +544,49 @@ def lista_caracteristicas_jefes():
     tabla = cur.fetchall()
 
     return jsonify(tabla)
+
+@app.route("/caracteristicas_jefe/<int:id>", methods=('DELETE',))
+def borrar_caracteristicas_jefe(id):
+    mari = mariadb.connect(
+        user = "minecraft",
+        password ="minecraft111",
+        host ="10.9.120.5",
+        database= "minecraft"
+    )
+    cur = mari.cursor(dictionary=True)
+    cur.execute("DELETE FROM Caracteristicas_jefe WHERE ID = %s", (id,))
+    mari.commit()
+
+    return jsonify({"resultado" : "ok",
+                    "id" : id})
+
+@app.route("/caracteristicas_jefe/", methods=('POST',))
+def agregar_caracteristicas_jefe():
+    mari = mariadb.connect(
+        user = "minecraft",
+        password ="minecraft111",
+        host ="10.9.120.5",
+        database= "minecraft"
+    )
+    cur = mari.cursor(dictionary=True)
+    nombre = request.json["nombre"]
+    efecto = request.json["efecto"]
+    daño_distacia = request.json["daño_distacia"]
+    daño_mele = request.json["daño_mele"]
+    ataque = request.json["ataque"]
+    consulta = """
+        INSERT INTO caracteristicas_jefe (nombre_ataque, afecto, daño_a_distacia, daño_mele, daño_del_ataque, jefes_ID ) VALUES (%s, %s, %s, %s, %s, %s)
+"""
+
+    cur.execute(consulta, (nombre,efecto,daño_distacia,daño_mele,ataque,id))
+    mari.commit()
+    id = cur.lastrowid
+
+    return jsonify({"resultado" : "ok",
+                    "id" : id})
+
+#====================================================
+#====================================================
 
 @app.route("/comida")
 def lista_comida():
@@ -360,6 +602,9 @@ def lista_comida():
 
     return jsonify(tabla)
 
+#====================================================
+#====================================================
+
 @app.route("/cubos")
 def lista_cubos():
     mari = mariadb.connect(
@@ -373,6 +618,9 @@ def lista_cubos():
     tabla = cur.fetchall()
 
     return jsonify(tabla)
+
+#====================================================
+#====================================================
 
 @app.route("/estructura")
 def lista_estructura():
@@ -388,6 +636,9 @@ def lista_estructura():
 
     return jsonify(tabla)
 
+#====================================================
+#====================================================
+
 @app.route("/herramientas_basicas")
 def lista_herramientas_basicas():
     mari = mariadb.connect(
@@ -401,6 +652,9 @@ def lista_herramientas_basicas():
     tabla = cur.fetchall()
 
     return jsonify(tabla)
+
+#====================================================
+#====================================================
 
 @app.route("/jefes")
 def lista_jefes():
@@ -416,6 +670,9 @@ def lista_jefes():
 
     return jsonify(tabla)
 
+#====================================================
+#====================================================
+
 @app.route("/jefes_dimension")
 def lista_jefes_dimension():
     mari = mariadb.connect(
@@ -430,6 +687,9 @@ def lista_jefes_dimension():
 
     return jsonify(tabla)
 
+#====================================================
+#====================================================
+
 @app.route("/jugador")
 def lista_jugador():
     mari = mariadb.connect(
@@ -443,6 +703,9 @@ def lista_jugador():
     tabla = cur.fetchall()
 
     return jsonify(tabla)
+
+#====================================================
+#====================================================
 
 @app.route("/zombie_salvador")
 def lista_zombie_salvador():
