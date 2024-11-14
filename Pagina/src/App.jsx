945f30d4cc647eb6e98f9de53f22a3cae762398f
gabrielamayta <gabrielamayta2109@gmail.com>
2024-11-14 12:01:50 -0300

    Css avanzado, falta llamar los datos

--- Pagina/src/App.jsx
+++ Pagina/src/App.jsx
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
-import { Link } from "react-router-dom";
 
 const App = () => {
-  //const [count, setCount] = useState(0)
-  const [view, setView] = useState({});
-
+  const [view, setView] = useState("inicio");
   const [data, setData] = useState({
     jugador: [],
     herramientas_basicas: [],
@@ -25,51 +22,39 @@ const App = () => {
   });
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/jugador")
-      .then((data) => data.json())
-      .then((response) => setJugador(response));
-    fetch("http://127.0.0.1:5000/herramientas_basicas")
-      .then((data) => data.json())
-      .then((response) => setHerramientas(response));
-    fetch("http://127.0.0.1:5000/armadura")
-      .then((data) => data.json())
-      .then((response) => setArmadura(response));
-    fetch("http://127.0.0.1:5000/encantamientos")
-      .then((data) => data.json())
-      .then((response) => setEncantamientos(response));
-    fetch("http://127.0.0.1:5000/comida")
-      .then((data) => data.json())
-      .then((response) => setComida(response));
-    fetch("http://127.0.0.1:5000/mob_pass")
-      .then((data) => data.json())
-      .then((response) => setMob_pass(response));
-    fetch("http://127.0.0.1:5000/mob_neutro")
-      .then((data) => data.json())
-      .then((response) => setMob_neutro(response));
-    fetch("http://127.0.0.1:5000/mob_hoss")
-      .then((data) => data.json())
-      .then((response) => setMob_hoss(response));
-    fetch("http://127.0.0.1:5000/jefes")
-      .then((data) => data.json())
-      .then((response) => setJefes(response));
-    fetch("http://127.0.0.1:5000/caracteristicas_jefe")
-      .then((data) => data.json())
-      .then((response) => setCaracteristicas_jefe(response));
-    fetch("http://127.0.0.1:5000/bioma")
-      .then((data) => data.json())
-      .then((response) => setBioma(response));
-    fetch("http://127.0.0.1:5000/estructura")
-      .then((data) => data.json())
-      .then((response) => setEstructura(response));
-    fetch("http://127.0.0.1:5000/agricultura")
-      .then((data) => data.json())
-      .then((response) => setAgricultura(response));
-    fetch("http://127.0.0.1:5000/dimension")
-      .then((data) => data.json())
-      .then((response) => setDimension(response));
-    fetch("http://127.0.0.1:5000/cubos")
-      .then((data) => data.json())
-      .then((response) => setCubos(response));
+    // Lista de rutas para obtener los datos desde Flask
+    const routes = [
+      "jugador",
+      "herramientas_basicas",
+      "armadura",
+      "encantamientos",
+      "comida",
+      "mob_pass",
+      "mob_neutro",
+      "mob_hoss",
+      "jefes",
+      "caracteristicas_jefe",
+      "bioma",
+      "estructura",
+      "dimension",
+      "agricultura",
+      "cubos",
+    ];
+
+    // Fetching de todos los datos
+    Promise.all(
+      routes.map((route) =>
+        fetch(`http://127.0.0.1:5000/${route}`)
+          .then((res) => res.json())
+          .then((json) => ({ [route]: json }))
+      )
+    ).then((results) => {
+      const newData = results.reduce(
+        (acc, current) => ({ ...acc, ...current }),
+        {}
+      );
+      setData(newData);
+    });
   }, []);
 
   const renderTable = (title, items) => (
@@ -98,23 +83,24 @@ const App = () => {
   return (
     <>
       <header className="header">
-        <p>Minecraft</p>
+        <h1>Minecraft</h1>
+        <img src="/Componentes/Objetos/Site-logo.webp" alt="" className="" />
         <nav className="navbar">
-          <button onClick={() => setView("inicio")}></button>
-          <button onClick={() => setView("jugador")}></button>
-          <button onClick={() => setView("mobs")}></button>
-          <button onClick={() => setView("jefes")}></button>
-          <button onClick={() => setView("biomas")}></button>
-          <button onClick={() => setView("estructura")}></button>
-          <button onClick={() => setView("dimension")}></button>
-          <button onClick={() => setView("agricultura")}></button>
-          <button onClick={() => setView("cubos")}></button>
+          <button onClick={() => setView("inicio")}>Inicio</button>
+          <button onClick={() => setView("jugador")}>Jugador</button>
+          <button onClick={() => setView("mobs")}>Mobs</button>
+          <button onClick={() => setView("jefes")}>Jefes</button>
+          <button onClick={() => setView("biomas")}>Biomas</button>
+          <button onClick={() => setView("estructura")}>Estructura</button>
+          <button onClick={() => setView("dimension")}>Dimensiones</button>
+          <button onClick={() => setView("agricultura")}>Agricultura</button>
+          <button onClick={() => setView("cubos")}>Cubos</button>
         </nav>
       </header>
 
-      <main>
+      <main className="body">
         {view === "inicio" && (
-          <main className="body">
+          <>
             {renderTable("jugador", data.jugador)}
             {renderTable("herramientas_basicas", data.herramientas_basicas)}
             {renderTable("armadura", data.armadura)}
@@ -130,66 +116,46 @@ const App = () => {
             {renderTable("dimension", data.dimension)}
             {renderTable("agricultura", data.agricultura)}
             {renderTable("cubos", data.cubos)}
-          </main>
+          </>
         )}
 
         {view === "jugador" && (
-          <main className="body">
+          <>
             {renderTable("jugador", data.jugador)}
             {renderTable("herramientas_basicas", data.herramientas_basicas)}
             {renderTable("armadura", data.armadura)}
             {renderTable("encantamientos", data.encantamientos)}
-          </main>
+          </>
         )}
 
         {view === "mobs" && (
-          <main className="body">
+          <>
             {renderTable("mob_pass", data.mob_pass)}
             {renderTable("mob_neutro", data.mob_neutro)}
             {renderTable("mob_hoss", data.mob_hoss)}
-          </main>
+          </>
         )}
 
         {view === "jefes" && (
-          <main className="body">
+          <>
             {renderTable("jefes", data.jefes)}
             {renderTable("caracteristicas_jefe", data.caracteristicas_jefe)}
-          </main>
+          </>
         )}
 
-        {view === "biomas" && (
-          <main className="body">{renderTable("bioma", data.bioma)}</main>
-        )}
-
-        {view === "estructura" && (
-          <main className="body">
-            {renderTable("estructura", data.estructura)}
-          </main>
-        )}
-
-        {view === "dimension" && (
-          <main className="body">
-            {renderTable("dimension", data.dimension)}
-          </main>
-        )}
-
-        {view === "agricultura" && (
-          <main className="body">
-            {renderTable("agricultura", data.agricultura)}
-          </main>
-        )}
-
-        {view === "cubos" && (
-          <main className="body">{renderTable("cubos", data.cubos)}</main>
-        )}
+        {view === "biomas" && renderTable("bioma", data.bioma)}
+        {view === "estructura" && renderTable("estructura", data.estructura)}
+        {view === "dimension" && renderTable("dimension", data.dimension)}
+        {view === "agricultura" && renderTable("agricultura", data.agricultura)}
+        {view === "cubos" && renderTable("cubos", data.cubos)}
       </main>
 
       <footer className="footer">
         <p>Creado por X</p>
         <div className="icons">
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
+          <a href="#">Icono 1</a>
+          <a href="#">Icono 2</a>
+          <a href="#">Icono 3</a>
         </div>
       </footer>
     </>
